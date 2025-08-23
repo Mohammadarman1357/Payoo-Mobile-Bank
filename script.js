8469daf6c70ca04bd94a5ab4c465c05f411fb559
Arman <mohammadjahedulislamarman@gmail.com>
2025-08-23 20:23:24 +0600

    payoo mobile bank web app making with tailwind css. added the login section and responsive for mobile

---
+++ script.js
@@ -0,0 +1,8 @@
+// login button functionality
+
+document.getElementById("loginButton").
+addEventListener("click",function(e){
+    e.preventDefault()
+    console.log('hi')
+    console.log(e)
+})
